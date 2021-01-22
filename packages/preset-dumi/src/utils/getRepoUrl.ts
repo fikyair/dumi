94b487d06c4d48b61a0071488a04709ac05b1ad0
fikyair <shiming_xsm@163.com>
2021-01-22 15:54:24 +0800

    fix: demo 编辑 url 兼容 gitlab 子项目

--- packages/preset-dumi/src/utils/getRepoUrl.ts
+++ packages/preset-dumi/src/utils/getRepoUrl.ts
@@ -4,7 +4,7 @@ export default (url: string) => {
   let repoUrl = hostedGit.fromUrl(url)?.browse();
 
   if (!repoUrl && url) {
-    repoUrl = url.replace(/^.*?((?:[\w-]+\.?)+)+[:/]([\w-]+)\/([\w-]+).*$/, 'https://$1/$2/$3');
+    repoUrl = url.replace(/^.*?((?:[\w-]+\.?)+)+[:/]([\w-]+)\/([\w-]+)\/([\w-]+).*$/, 'https://$1/$2/$3/$4');
   }
 
   return repoUrl;
